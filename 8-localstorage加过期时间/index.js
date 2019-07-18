0e9feef274357e31f20377180003a62cccf6723c
王晋杰 <2310385247@qq.com>
2019-07-18 17:39:47 +0800

    localstorage增加过期时间

---
+++ 8-localstorage加过期时间/index.js
@@ -0,0 +1,26 @@
+//github.com/Advanced-Frontend/Daily-Interview-Question/issues/171
+https:;
+
+;(function() {
+    var getItem = localStorage.getItem.bind(localStorage)
+    var setItem = localStorage.setItem.bind(localStorage)
+    var removeItem = localStorage.removeItem.bind(localStorage)
+    localStorage.getItem = function(keyName) {
+        var expires = getItem(keyName + '_expires')
+        if (expires && new Date() > new Date(Number(expires))) {
+            removeItem(keyName)
+            removeItem(keyName + '_expires')
+        }
+        return getItem(keyName)
+    }
+    localStorage.setItem = function(keyName, keyValue, expires) {
+        if (typeof expires !== 'undefined') {
+            var expiresDate = new Date(expires).valueOf()
+            setItem(keyName + '_expires', expiresDate)
+        }
+        return setItem(keyName, keyValue)
+    }
+})()
+
+localStorage.setItem('key', 'value', Date.now() + 180000)
+localStorage.getItem('key')
