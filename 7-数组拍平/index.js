95245e540edc280783aeeced8d67a69197bd2e8c
王晋杰 <2310385247@qq.com>
2019-07-18 14:43:08 +0800

    数组拍平

--- 7-数组拍平/index.js
+++ 7-数组拍平/index.js
@@ -2,6 +2,10 @@
 
 let arr = [1, 2, [3, 4, 5, [6, 7], 8], 9, 10, [11, [12, 13]]]
 
+/**
+ * 方法1
+ * @param {arr} arr
+ */
 const flatten = function(arr) {
     while (arr.some(item => Array.isArray(item))) {
         console.log('arr', arr)
@@ -11,3 +15,32 @@ const flatten = function(arr) {
 }
 
 console.log(flatten(arr))
+
+/**
+ * 方法2
+ */
+const flatten2 = arr
+    .join(',')
+    .split(',')
+    .map(Number)
+console.log(flatten2)
+
+/**
+ * 方法3
+ * @param {arr} arr
+ */
+function flatten3(arr) {
+    let arrs = [...arr]
+    let newArr = []
+    while (arrs.length) {
+        let item = arrs.shift()
+        if (Array.isArray(item)) {
+            arrs.unshift(...item)
+        } else {
+            newArr.push(item)
+        }
+    }
+    return newArr
+}
+
+console.log(flatten3(arr))
